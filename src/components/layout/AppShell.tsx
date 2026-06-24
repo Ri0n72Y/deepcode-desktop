d9cc71aa0403febd9b3a82a6718df4ea10ec926d
Ri0n72Y <tzy673570751@gmail.com>
2026-06-25 07:56:12 +0800

    Use replicated shell

--- src/components/layout/AppShell.tsx
+++ src/components/layout/AppShell.tsx
@@ -1,23 +1,11 @@
-import ChatPanel from "../chat/ChatPanel";
-import ModelPanel from "../model/ModelPanel";
-import PermissionPanel from "../permissions/PermissionPanel";
-import ProcessPanel from "../processes/ProcessPanel";
-import RuntimeStatusBar from "../runtime/RuntimeStatusBar";
-import SessionSidebar from "../sessions/SessionSidebar";
+import Conversation from "../extension/Conversation";
+import HeaderBar from "../extension/HeaderBar";
 
 export default function AppShell() {
   return (
-    <main className="flex h-screen w-screen flex-col overflow-hidden bg-[#0f1117] text-slate-100">
-      <div className="flex min-h-0 flex-1">
-        <SessionSidebar />
-        <ChatPanel />
-        <aside className="hidden w-80 shrink-0 flex-col gap-3 border-l border-white/10 bg-[#141821] p-4 xl:flex">
-          <ModelPanel />
-          <PermissionPanel />
-          <ProcessPanel />
-        </aside>
-      </div>
-      <RuntimeStatusBar />
+    <main className="app">
+      <HeaderBar />
+      <Conversation />
     </main>
   );
 }
