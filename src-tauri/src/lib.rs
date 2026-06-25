accf24a1a3e45927488f64733b9540a8f83f9642
Ri0n72Y <tzy673570751@gmail.com>
2026-06-25 08:43:35 +0800

    Register host-owned runtime commands

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,7 +1,18 @@
+mod deepcode_runtime;
+
+use deepcode_runtime::{deepcode_request, runtime_status, start_deepcode_server, stop_deepcode_server, DeepcodeRuntimeState};
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
+        .manage(DeepcodeRuntimeState::default())
+        .invoke_handler(tauri::generate_handler![
+            runtime_status,
+            start_deepcode_server,
+            stop_deepcode_server,
+            deepcode_request,
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
