b1f4ab79e1cfd7785f02e54dffa0d76fef165685
Ri0n72Y <tzy673570751@gmail.com>
2026-06-27 16:02:41 +0800

    Register static DeepCode file commands

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,6 +1,12 @@
+mod deepcode_static;
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![
+            deepcode_static::read_deepcode_history,
+            deepcode_static::read_deepcode_settings,
+        ])
         .plugin(tauri_plugin_opener::init())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
