8b101f06842622068377bafdc0abd10bad632e33
Ri0n72Y <tzy673570751@gmail.com>
2026-06-27 15:45:24 +0800

    Use chat container view in app shell

--- src/components/layout/AppShell.tsx
+++ src/components/layout/AppShell.tsx
@@ -1,11 +1,11 @@
-import ChatContainer from "../extension/ChatContainer";
+import ChatContainerView from "../extension/ChatContainerView";
 import HeaderBar from "../extension/HeaderBar";
 
 export default function AppShell() {
   return (
     <main className="app">
       <HeaderBar />
-      <ChatContainer />
+      <ChatContainerView />
     </main>
   );
 }
