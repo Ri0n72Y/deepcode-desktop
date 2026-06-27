6adf070d06ab359d4b0312a090a94ce0d9f3bdc8
Ri0n72Y <tzy673570751@gmail.com>
2026-06-27 16:35:02 +0800

    Register static session and skill readers

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -5,7 +5,9 @@ pub fn run() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![
             deepcode_static::read_deepcode_history,
+            deepcode_static::read_deepcode_session,
             deepcode_static::read_deepcode_settings,
+            deepcode_static::read_deepcode_skills,
         ])
         .plugin(tauri_plugin_opener::init())
         .run(tauri::generate_context!())
