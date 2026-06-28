2263a0ff5a18e000229520b21ec3707a82405128
Ri0n72Y <tzy673570751@gmail.com>
2026-06-28 20:30:30 +0800

    Add timeline tool visibility preference

--- src/stores/ui-preferences-store.ts
+++ src/stores/ui-preferences-store.ts
@@ -2,10 +2,14 @@ import { create } from "zustand";
 
 type UiPreferencesState = {
   showSkills: boolean;
+  showTools: boolean;
   setShowSkills: (value: boolean) => void;
+  setShowTools: (value: boolean) => void;
 };
 
 export const useUiPreferencesStore = create<UiPreferencesState>((set) => ({
   showSkills: false,
+  showTools: true,
   setShowSkills: (value) => set({ showSkills: value }),
+  setShowTools: (value) => set({ showTools: value }),
 }));
