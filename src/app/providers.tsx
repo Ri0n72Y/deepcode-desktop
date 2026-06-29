47284671915480940d4e9af25fae08f4d739d051
Ri0n72Y <tzy673570751@gmail.com>
2026-06-29 12:16:44 +0800

    PR4: Wire runtime provider through runtime client factory

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,6 +1,6 @@
 import { createContext, useContext, useEffect, useMemo, type ReactNode } from "react";
 import { dispatchRuntimeEvent } from "../lib/runtime/event-dispatcher";
-import { MockRuntimeClient } from "../lib/runtime/mock-runtime-client";
+import { createRuntimeClient } from "../lib/runtime/runtime-client";
 import type { RuntimeClient } from "../lib/runtime/types";
 
 const RuntimeClientContext = createContext<RuntimeClient | undefined>(undefined);
@@ -10,7 +10,7 @@ type RuntimeProviderProps = {
 };
 
 export function RuntimeProvider({ children }: RuntimeProviderProps) {
-  const client = useMemo(() => new MockRuntimeClient(), []);
+  const client = useMemo(() => createRuntimeClient(), []);
 
   useEffect(() => {
     const unsubscribe = client.subscribe(dispatchRuntimeEvent);
